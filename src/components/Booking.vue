95ea40ca1e5cf0777f4e8dab84cb80087443fbf6
LittleLemon666 <damoncho510@gmail.com>
2021-12-12 15:39:19 +0800

    Pop DateSelect
    Pop DateSelect

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -15,7 +15,7 @@
                         max-height="100%"
                         max-width="100%"
                         contain
-                        @click="Rent"
+                        @click="Popmenu"
                     >
                         <v-card class="embed-card" flat color="light-blue lighten-5" position="bottom right">
                             <v-subheader>Person {{ person }} </v-subheader>
@@ -24,6 +24,66 @@
                 </v-card>
             </v-row>
         </v-container>
+
+        <v-row
+            class="d-flex"
+            justify="center"
+        >
+        <v-menu
+            ref="menuVisible"
+            v-model="menuVisible"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            absolute
+            offset-y
+            :position-x="x"
+            :position-y="y"
+            min-width="auto"
+        >
+            <v-tabs
+                v-model="tab"
+                align-with-title
+                justify-end
+                >
+                <v-tabs-slider color="yellow"></v-tabs-slider>
+
+                <v-tab
+                    v-for="item in items"
+                    :key="item"
+                >
+                    {{ item }}
+                </v-tab>
+            </v-tabs>
+            <v-tabs-items v-model="tab">
+                <v-tab-item>
+                    <v-card flat>
+                        
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-date-picker
+                            v-model="date"
+                            no-title
+                            scrollable
+                            value="yyyy/MM/dd"
+                        >
+                            <v-layout justify-end row1>
+                                <v-btn
+                                    text
+                                    color="primary"
+                                    @click="$refs.menuVisible.save(date)"
+                                >
+                                    confirm
+                                </v-btn>
+                            </v-layout>
+                        </v-date-picker>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs-items>
+        </v-menu>
+        </v-row>
     </v-app>
 </template> 
 
@@ -31,7 +91,7 @@
 export default {
     data () {
         return {
-            dates: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+            date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
             roomNames: [
                 'RoomA',
                 'RoomB',
@@ -66,10 +126,24 @@ export default {
                     true,
                     true,
                 ],
-            ]
+            ],
+            menuVisible: false,
+            tab: null,
+            items: [
+                'Day', 'Month',
+            ],
+            x: 0,
+            y: 0
         };
     },
     methods: {
+        Popmenu (event) {
+            if (!this.menuVisible) {
+                this.x = event.pageX;
+                this.y = event.pageY;
+            }
+            this.menuVisible = true;
+        },
         Rent () {
             //this.$router.replace('/login')
             console.log(this.roomInfo)
